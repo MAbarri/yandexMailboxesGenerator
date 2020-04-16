cbf4fbea8506413a247791edd7bf06c62ba38bef
abarri <officiel.abarri@gmail.com>
2020-04-16 14:24:04 +0100

    minor fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  yandexClientID: "b8b47f25cb1b45718d36fc50cd392466",
+  godaddy: {
+    key: "3mM44Uah8XpP17_8HYur4RZ3zLcecZGNRnioE",
+    secret: "XRFTV247m3rqidsqZaRjoR"
+  }
 };
