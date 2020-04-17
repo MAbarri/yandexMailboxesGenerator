380f275216d882f500bd7fe103fbcefa16be0945
abarri <officiel.abarri@gmail.com>
2020-04-17 16:26:02 +0100

    changed to backedn calls

--- backend/routes/externalapi.route.js
+++ backend/routes/externalapi.route.js
@@ -3,7 +3,7 @@ const _ = require('underscore');
 const app = express();
 const subdomainRoute = express.Router();
 const https = require('https');
-
+var fs = require('fs');
 // Subdomain model
 let Subdomain = require('../models/Subdomain');
 
