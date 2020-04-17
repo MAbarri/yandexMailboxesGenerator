291e5b3da6154d252ed4cad31b9fb42b7de32cb5
abarri <officiel.abarri@gmail.com>
2020-04-17 16:59:52 +0100

    added axios

--- backend/routes/externalapi.route.js
+++ backend/routes/externalapi.route.js
@@ -3,7 +3,7 @@ const _ = require('underscore');
 const app = express();
 const subdomainRoute = express.Router();
 const https = require('https');
-https.globalAgent.options.secureProtocol = 'TLSv1_2_method'
+const axios = require('axios');
 var fs = require('fs');
 // Subdomain model
 let Subdomain = require('../models/Subdomain');
@@ -14,23 +14,33 @@ subdomainRoute.route('/makeExternalCall').post((req, res, next) => {
   // originHost.host = req.body.host;
   delete originHost.referer;
   delete originHost.host;
+  // var options = {
+  //   hostname: req.body.host,
+  //   port: 80,
+  //   path: req.body.path,
+  //   method: req.body.method,
+  //   headers: req.body.headers,
+  //   body: req.body.body,
+  //   rejectUnauthorized: false
+  // };
   var options = {
-    hostname: req.body.host,
+    url: "https://"+req.body.host+req.body.path,
     port: 80,
-    path: req.body.path,
     method: req.body.method,
     headers: req.body.headers,
     body: req.body.body,
     rejectUnauthorized: false
   };
   options.headers['User-Agent'] = 'curl/7.21.4 (universal-apple-darwin11.0) libcurl/7.21.4 OpenSSL/0.9.8r zlib/1.2.5';
-  console.log('options', options)
-  https.request(options, function(httpres) {
-    httpres.setEncoding('utf8');
-    httpres.on('data', function (chunk) {
-      res.json(chunk)
-    });
-  }).end();
+  axios(options)
+  .then(function (response) {
+
+      res.json(response.data);
+  })
+  .catch(function (error) {
+        res.json(error.response.data);
+  });
+
 });
 
 module.exports = subdomainRoute;
